d5841db6b148d4dc449b65343a858aab04de21b6
SergeyY1994 <serrrrrrrrr@yandex.ru>
2022-11-30 10:42:44 +0300

    Добавляет доработки для мобильного меню

--- source/js/modules/mobile-menu.js
+++ source/js/modules/mobile-menu.js
@@ -3,33 +3,53 @@ const navToggle = document.querySelector('.main-navigation__toggle');
 const mainHeaderContainer = document.querySelector('.main-header__container');
 const mainHeaderNavigationList = document.querySelector('.main-header__container .main-navigation__list');
 const mainHeaderLogoElement = document.querySelector('.main-header__logo');
-const mainHeaderLogoImgElement = document.querySelector('.main-header__logo-img');
+const mobileMenuLinkNodeList = document.querySelectorAll('.main-navigation__link--mobile-menu');
 const wrapperElement = document.querySelector('.wrapper');
+const mainHeaderSourceElement = document.querySelector('.main-header__logo-link source');
 
-if (navMain && navToggle && mainHeaderContainer && mainHeaderNavigationList && mainHeaderLogoElement && mainHeaderLogoImgElement) {
+if (navMain && navToggle && mainHeaderContainer && mainHeaderNavigationList && mainHeaderLogoElement && mainHeaderSourceElement && mobileMenuLinkNodeList) {
   navMain.classList.remove('main-navigation--nojs');
+
+  if (window.matchMedia('(max-width:767px)').matches) {
+    document.querySelector('source').setAttribute('srcset', '../img/svg/antartic-logo.svg');
+  }
 
   navToggle.addEventListener('click', function () {
     if (navMain.classList.contains('main-navigation--closed')) {
-      if (wrapperElement) {
-        wrapperElement.classList.add('menu-open');
-      }
-
-      mainHeaderNavigationList.style.paddingBottom = '300px';
-      mainHeaderLogoElement.style.left = '51px';
-      mainHeaderLogoImgElement.setAttribute('src', '../img/svg/antarctic-logo-mobile.svg');
-      navMain.classList.remove('main-navigation--closed');
-      navMain.classList.add('main-navigation--opened');
+      openMobileMenu();
     } else {
-      if (wrapperElement) {
-        wrapperElement.classList.remove('menu-open');
-      }
-
-      mainHeaderNavigationList.style.paddingBottom = '0';
-      mainHeaderLogoElement.style.left = '17px';
-      mainHeaderLogoImgElement.setAttribute('src', '../img/svg/antartic-logo.svg');
-      navMain.classList.add('main-navigation--closed');
-      navMain.classList.remove('main-navigation--opened');
+      closeMobileMenu();
     }
   });
+
+  for (let element of mobileMenuLinkNodeList) {
+    if (window.matchMedia('(max-width:767px)').matches) {
+      element.addEventListener('click', closeMobileMenu);
+    }
+  }
+
+}
+
+function openMobileMenu() {
+  if (wrapperElement) {
+    wrapperElement.classList.add('menu-open');
+  }
+
+  mainHeaderNavigationList.style.paddingBottom = '300px';
+  mainHeaderLogoElement.style.left = '51px';
+  mainHeaderSourceElement.setAttribute('srcset', '../img/svg/antarctic-logo-mobile.svg');
+  navMain.classList.remove('main-navigation--closed');
+  navMain.classList.add('main-navigation--opened');
+}
+
+export function closeMobileMenu() {
+  if (wrapperElement) {
+    wrapperElement.classList.remove('menu-open');
+  }
+
+  mainHeaderNavigationList.style.paddingBottom = '0';
+  mainHeaderLogoElement.style.left = '17px';
+  mainHeaderSourceElement.setAttribute('srcset', '../img/svg/antartic-logo.svg');
+  navMain.classList.add('main-navigation--closed');
+  navMain.classList.remove('main-navigation--opened');
 }
--- source/js/modules/stretching-background-on-main-block.js
+++ source/js/modules/stretching-background-on-main-block.js
@@ -1,10 +1,19 @@
 const mainBlockElement = document.querySelector('.main-block');
 const mainHeaderElement = document.querySelector('.main-header');
 const navToggle = document.querySelector('.main-navigation__toggle');
+const mobileMenuLinkNodeList = document.querySelectorAll('.main-navigation__link--mobile-menu');
 
 changeMainBlockMargin();
 
 navToggle.addEventListener('click', changeMainBlockMargin);
+
+if (mobileMenuLinkNodeList) {
+  for (let element of mobileMenuLinkNodeList) {
+    if (window.matchMedia('(max-width:767px)').matches) {
+      element.addEventListener('click', changeMainBlockMargin);
+    }
+  }
+}
 
 function changeMainBlockMargin() {
   if (mainBlockElement && mainHeaderElement) {
