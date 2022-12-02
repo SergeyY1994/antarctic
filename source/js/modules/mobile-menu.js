e637fa8a58ec752a63710ff974d5d4a34482565f
SergeyY1994 <serrrrrrrrr@yandex.ru>
2022-12-02 17:24:42 +0300

    Добавляет исправления

--- source/js/modules/mobile-menu.js
+++ source/js/modules/mobile-menu.js
@@ -3,19 +3,15 @@ import {changeMainBlockMargin} from './stretching-background-on-main-block';
 const bodyElement = document.body;
 const navMain = document.querySelector('.main-navigation');
 const navToggle = document.querySelector('.main-navigation__toggle');
-const mainHeaderContainer = document.querySelector('.main-header__container');
-const mainHeaderNavigationList = document.querySelector('.main-header__container .main-navigation__list');
-const mainHeaderLogoElement = document.querySelector('.main-header__logo');
 const mobileMenuLinkNodeList = document.querySelectorAll('.main-navigation__link--mobile-menu');
 const wrapperElement = document.querySelector('.wrapper');
 const mainHeaderSourceElement = document.querySelector('.main-header__logo-link source');
 
-if (navMain && navToggle && mainHeaderContainer && mainHeaderNavigationList && mainHeaderLogoElement && mainHeaderSourceElement && mobileMenuLinkNodeList && wrapperElement) {
+if (navMain && navToggle && mainHeaderSourceElement && mobileMenuLinkNodeList && wrapperElement) {
   navMain.classList.remove('main-navigation--nojs');
 
   if (window.matchMedia('(max-width:767px)').matches) {
-    // переписать здесь на mainHeaderSourceElement
-    document.querySelector('source').setAttribute('srcset', '../img/svg/antartic-logo.svg');
+    mainHeaderSourceElement.setAttribute('srcset', '../img/svg/antartic-logo.svg');
   }
 
   navToggle.addEventListener('click', function () {
@@ -46,8 +42,6 @@ function openMobileMenu() {
   }
 
   bodyElement.style.overflowY = 'hidden';
-  mainHeaderNavigationList.style.paddingBottom = '300px';
-  mainHeaderLogoElement.style.left = '51px';
   mainHeaderSourceElement.setAttribute('srcset', '../img/svg/antarctic-logo-mobile.svg');
   navMain.classList.remove('main-navigation--closed');
   navMain.classList.add('main-navigation--opened');
@@ -60,8 +54,6 @@ function closeMobileMenu() {
   }
 
   bodyElement.style.overflowY = 'auto';
-  mainHeaderNavigationList.style.paddingBottom = '0';
-  mainHeaderLogoElement.style.left = '17px';
   mainHeaderSourceElement.setAttribute('srcset', '../img/svg/antartic-logo.svg');
   navMain.classList.add('main-navigation--closed');
   navMain.classList.remove('main-navigation--opened');
